ac650bbf5d69dddc3948d7dfe19563b502cc039d
austinandika <austinandika@gmail.com>
2021-05-19 18:54:25 +0700

    Fix variable name

--- JS/Register-Validation.js
+++ JS/Register-Validation.js
@@ -12,13 +12,6 @@ var lblErrorEmail = document.getElementById('lbl-error-email');
 var lblErrorPassword = document.getElementById('lbl-error-password');
 var lblErrorConfirmPass = document.getElementById('lbl-error-confirm-password');
 
-var effectErrorEmail = document.getElementById('txt-email');
-var effectErrorName = document.getElementById('txt-name');
-var effectErrorPassword = document.getElementById('txt-password');
-var effectErrorConfirmPass = document.getElementById('txt-confirm-password');
-var effectErrorTermCondition = document.getElementById("cb-term-condition-container");
-
-
 var flag = true;
 form.addEventListener('submit', (e) => {
 
@@ -50,21 +43,21 @@ function resetLblError() {
 }
 
 function resetEffectError() {
-    effectErrorEmail.classList.remove("error");
-    effectErrorName.classList.remove("error");
-    effectErrorPassword.classList.remove("error");
-    effectErrorConfirmPass.classList.remove("error");
-    effectErrorTermCondition.classList.remove("error");
+    txtEmail.classList.remove("error");
+    txtName.classList.remove("error");
+    txtPassword.classList.remove("error");
+    txtConfirmPass.classList.remove("error");
+    cbTermCondition.classList.remove("error");
 }
 
 function validateName(){
     if (txtName.value === '' || txtName.value == null) {
         lblErrorName.innerHTML = "Name is required";
-        effectErrorName.classList.add("error");
+        txtName.classList.add("error");
         flag = false;
     } else if (txtName.value.length < 3) {
         lblErrorName.innerHTML = "Name length must be at least 3 characters";
-        effectErrorName.classList.add("error");
+        txtName.classList.add("error");
         flag = false;
     }
 }
@@ -89,17 +82,17 @@ function validateEmail(){
 
     if(txtEmail.value.length == 0){
         lblErrorEmail.innerHTML = "Email is required";
-        effectErrorEmail.classList.add("error");
+        txtEmail.classList.add("error");
         isValidEmail = false;
     }
     else if(txtEmail.value.length < 2){
         lblErrorEmail.innerHTML = "Please enter a valid email address";
-        effectErrorEmail.classList.add("error");
+        txtEmail.classList.add("error");
         isValidEmail = false;
     }
     else if (atEmail == -1) {   // No '@' at email address
         lblErrorEmail.innerHTML = "Please enter a valid email address";
-        effectErrorEmail.classList.add("error");
+        txtEmail.classList.add("error");
         isValidEmail = false;
     } 
 
@@ -111,14 +104,14 @@ function validateEmail(){
     
         if(dotEmail == -1 || dotEmail < 2 || dotCount > 2){ // at least one dot, max 2 dot after "@" and minimum 1 character after "@"
             lblErrorEmail.innerHTML = "Please enter a valid email address";
-            effectErrorEmail.classList.add("error");
+            txtEmail.classList.add("error");
             isValidEmail = false;
         }
         
         for (var i = 0; i < dotSplits.length; i++) { // dot cannot be the last of the character and dots are not repeated
             if (dotSplits[i].length == 0) {
                 lblErrorEmail.innerHTML = "Please enter a valid email address";
-                effectErrorEmail.classList.add("error");
+                txtEmail.classList.add("error");
                 isValidEmail = false;
             }
         }
@@ -132,11 +125,11 @@ function validateEmail(){
 function validatePassword(){
     if (txtPassword.value === '' || txtPassword.value == null) {
         lblErrorPassword.innerHTML = "Password is required";
-        effectErrorPassword.classList.add("error");
+        txtPassword.classList.add("error");
         flag = false;
     } else if (txtPassword.value.length <= 8) {
         lblErrorPassword.innerHTML = "Password length must be at least 8 characters";
-        effectErrorPassword.classList.add("error");
+        txtPassword.classList.add("error");
         flag = false;
     }
 }
@@ -144,22 +137,22 @@ function validatePassword(){
 function validateConfirmPass(){
     if (txtConfirmPass.value === '' || txtConfirmPass.value == null) {
         lblErrorConfirmPass.innerHTML = "Confirm password is required";
-        effectErrorConfirmPass.classList.add("error");
+        txtConfirmPass.classList.add("error");
         flag = false;
     } else if (txtConfirmPass.value.length <= 8) {
         lblErrorConfirmPass.innerHTML = "Password length must be at least 8 characters";
-        effectErrorConfirmPass.classList.add("error");
+        txtConfirmPass.classList.add("error");
         flag = false;
     }else if (txtConfirmPass.value != txtPassword.value) {
         lblErrorConfirmPass.innerHTML = "Password didn't match";
-        effectErrorConfirmPass.classList.add("error");
+        txtConfirmPass.classList.add("error");
         flag = false;
     } 
 }
 
 function validateTermCondition(){
     if (cbTermCondition.checked == false) {
-        effectErrorTermCondition.classList.add("error");
+        cbTermCondition.classList.add("error");
         flag = false;
     }
 }
